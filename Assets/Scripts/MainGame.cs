27dce7c7917b386ba7061a71d929753c62d9fe30
Nekot94 <nekomato9@gmail.com>
2017-06-10 11:20:16 +0300

    123

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum States
+{
+    strangeRoom,
+    rosmarus,
+    anotherDimension,
+    codeWorld,
+    street,
+    fightWithBear,
+    goodEnd
+}
+
+public class MainGame : MonoBehaviour
+{
+    public Text mainText;
+
+    private States myState;
+
+    private void Start()
+    {
+        myState = States.strangeRoom;
+    }
+
+    private void Update()
+    {
+        if (myState == States.strangeRoom)
+            StrangeRoom();
+        if (myState == States.rosmarus)
+            Rosmarus();
+    }
+
+    private void StrangeRoom()
+    {
+        mainText.text = "Вы в странной комнате. Почему она странная?\n" +
+            "В углу комнаты лежит телефон с надписью голосовать за Нэвэльного\n\n" +
+            "Голосовать за Нэвэльного (Г)\n" +
+            "Подпрыгнуть (П)\n";
+
+        if (Input.GetKeyDown(KeyCode.U))
+            myState = States.rosmarus;
+    }
+
+    private void Rosmarus()
+    {
+        mainText.text = "Как только вы голосуете, появляются танцующий морж.\n" +
+            "Он плюет бивнем вам в лицо, и вы умираете.\n\n" +
+            "КОНЕЦ";
+    }
+
+
+
+}

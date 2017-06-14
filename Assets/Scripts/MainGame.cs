578925e5d8293d30f3fab9a728148ea69d78a6a9
Nekot94 <nekomato9@gmail.com>
2017-06-14 12:59:37 +0300

    21

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -9,7 +9,6 @@ public enum States
     rosmarus,
     anotherDimension,
     codeWorld,
-    street,
     fightWithBear,
     goodEnd
 }
@@ -31,6 +30,12 @@ public class MainGame : MonoBehaviour
             StrangeRoom();
         if (myState == States.rosmarus)
             Rosmarus();
+        if (myState == States.anotherDimension)
+            AnotherDimension();
+        if (myState == States.fightWithBear)
+            FightWithBear();
+        if (myState == States.goodEnd)
+            GoodEnd();
     }
 
     private void StrangeRoom()
@@ -38,10 +43,14 @@ public class MainGame : MonoBehaviour
         mainText.text = "Вы в странной комнате. Почему она странная?\n" +
             "В углу комнаты лежит телефон с надписью голосовать за Нэвэльного\n\n" +
             "Голосовать за Нэвэльного (Г)\n" +
-            "Подпрыгнуть (П)\n";
+            "Подпрыгнуть (П)\n" +
+            "";
 
         if (Input.GetKeyDown(KeyCode.U))
             myState = States.rosmarus;
+
+        if (Input.GetKeyDown(KeyCode.G))
+            myState = States.anotherDimension;
     }
 
     private void Rosmarus()
@@ -51,6 +60,31 @@ public class MainGame : MonoBehaviour
             "КОНЕЦ";
     }
 
+    private void AnotherDimension()
+    {
+        mainText.text = "Вы прыгаете так высоко, что улетаете в стратосферу и попадаете \n" +
+            "в другое измерение." +
+            "\n(н)-Присесть за берёзку" +
+            "\n(в)-Прыгнуть ещё выше" +
+            "\n(ы)-Феличита я вышел на улицу";
 
+        if (Input.GetKeyDown(KeyCode.Y))
+            myState = States.strangeRoom;
 
+        if (Input.GetKeyDown(KeyCode.S))
+            myState = States.fightWithBear;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            myState = States.goodEnd;
+    }
+
+    private void FightWithBear()
+    {
+        mainText.text = "На вас напала курица, вы не пережили";
+    }
+
+    private void GoodEnd()
+    {
+        mainText.text = "Вы улетели в рай. Хорошая коновка.";
+    }
 }
